d02b399156ee108b9fbddb6ce83b58dffbe1179f
Armine <alexhollohan@gmail.com>
2017-11-23 18:38:52 -0500

    trying to get a pop up not worrking well will need to look into more later

---
+++ Prototype/Assets/Scripts/HomeScreen.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using System;
+
+public class HomeScreen : MonoBehaviour {
+
+    public Sprite img_popUp;
+    public Sprite img_mirror;
+
+
+    // Use this for initialization
+    void Start () {
+        //img_popUp.enabled = false;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void mirroPopUp ()
+    {
+
+
+    }
+}
