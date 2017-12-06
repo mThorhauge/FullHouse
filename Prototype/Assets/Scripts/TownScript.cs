a1fa24fdc4e728619d41122ef76391a828608402
Nicolemackin <nicole.mackin@carleton.ca>
2017-12-06 10:51:31 -0500

    no message

--- Prototype/Assets/Scripts/TownScript.cs
+++ Prototype/Assets/Scripts/TownScript.cs
@@ -53,6 +53,12 @@ public class TownScript : MonoBehaviour {
 
     }
 
+	public void toHomeClick() {
+
+		SceneManager.LoadScene("Home", LoadSceneMode.Single);
+
+	}
+
     public void upgradeBuildingClick(int buildingID) {
 
 		//Switch between chosen building
