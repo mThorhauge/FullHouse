891ad840b04f8b5ff6170916c5ee24e8e0380c01
Anjali Dutt <anjalidutt@yahoo.com>
2017-12-07 17:34:02 -0500

    prestiging resets building levels

--- Prototype/Assets/Scripts/PrestigeScript.cs
+++ Prototype/Assets/Scripts/PrestigeScript.cs
@@ -18,6 +18,7 @@ public class PrestigeScript : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+        //display the current prestige level as number of bitizens
         prestigeDisplay.text = gameStates.PrestigeLvl + " Bitizens";
 	}
 
@@ -25,13 +26,20 @@ public class PrestigeScript : MonoBehaviour {
     {
         if (currentBits >= 10) //low for testing purposes
         {
+            //add all the building levels together
             totalBuildingLevels = gameStates.ApothecaryLvl + gameStates.BlacksmithLvl + gameStates.TavernLvl + gameStates.FortuneTellerLvl;
 
-            prestigeLevel += (totalBuildingLevels + currentBits) / 4;
+            prestigeLevel += (totalBuildingLevels + currentBits) / 10;//added prestige is a percentage of building levels and bits
+           
             //need to reset bits and building levels
             currentBits = 0;
             gameStates.Bits = 0;
             gameStates.PrestigeLvl = prestigeLevel;
+
+            gameStates.ApothecaryLvl = 1;
+            gameStates.BlacksmithLvl = 1;
+            gameStates.TavernLvl = 1;
+            gameStates.FortuneTellerLvl = 1;
 
         }
     }
