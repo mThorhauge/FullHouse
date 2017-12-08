b3c1ac6c12daa3b8167c37edc27b3cb2f0799a2d
Nicolemackin <nicole.mackin@carleton.ca>
2017-12-07 21:47:55 -0500

    Add back button from store, add chunk options to store

--- Prototype/Assets/Scripts/StoreScript.cs
+++ Prototype/Assets/Scripts/StoreScript.cs
@@ -21,7 +21,23 @@ public class StoreScript : MonoBehaviour {
 		bitsCountDisplay.text ="" + gameStates.Bits;
 		monsterDropCountDisplay.text = "" + gameStates.MonsterDrops;
 		chunkCountDisplay.text = "" + gameStates.Chunks;
+	}
 
+	//since we can't actually charge you money, please take it for free
+	public void purchaseOffer1() {
+		gameStates.Chunks += 10;
+	}
+
+	public void purchaseOffer2() {
+		gameStates.Chunks += 25;
+	}
+
+	public void purchaseOffer3() {
+		gameStates.Chunks += 50;
+	}
+
+	public void purchaseOffer4() {
+		gameStates.Chunks += 75;
 	}
 
 	public void toHomeClicked()
@@ -32,7 +48,7 @@ public class StoreScript : MonoBehaviour {
 			SceneManager.LoadScene ("Home", LoadSceneMode.Single);
 		} else if (gameStates.LastScene == 3) {
 			SceneManager.LoadScene ("Dungeon", LoadSceneMode.Single);
-		} else {
+		} else if (gameStates.LastScene == 0) {
 			SceneManager.LoadScene ("Town", LoadSceneMode.Single);
 		}
 	}
